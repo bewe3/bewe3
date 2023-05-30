08967c343dd1511d02b754d15302bd9fded54306
Bryan Lynn Welton III <weltontrain@gmail.com>
2023-05-30 10:47:08 -0600

    mongodb templating

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+const cors = require('cors');
+app.use(cors({
+    origin: '*',
+}));
+
+app
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        next();
+    })
+    .use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port);
+        console.log(`Connected to and listening on ${port}`);
+    }
+});
