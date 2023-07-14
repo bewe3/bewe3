f54e7ccf57d4bc6f74fca296af36ed57ac04412d
Bryan Lynn Welton III <weltontrain@gmail.com>
2023-07-14 09:55:33 -0600

    fixed file paths

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'pages/*')));
-app.use(express.static(path.join(__dirname, 'styles/*')));
-app.use(express.static(path.join(__dirname, 'images/*')));
+app.use(express.static(path.join(__dirname, 'pages')));
+app.use(express.static(path.join(__dirname, 'styles')));
+app.use(express.static(path.join(__dirname, 'images')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
